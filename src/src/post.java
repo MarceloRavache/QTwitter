a0823416c1fd13efeadfd669f45df9c551f85eaf
MarceloRavache <32018314+MarceloRavache@users.noreply.github.com>
2018-06-19 09:05:22 -0300

    Add files via upload

---
+++ src/src/post.java
@@ -0,0 +1,27 @@
+
+package src;
+
+/**
+ *
+ * @author sytoo
+ */
+
+public class post {
+    public String nome;
+    public String conteudo;
+    public String id;
+    public String id_post;
+
+    public void setnome(String nome){
+        this.nome = nome;
+    }
+    public void setconteudo(String conteudo){
+        this.conteudo =  conteudo;
+    }
+    public void setid(String id){
+        this.id = id;
+    }
+    public void setid_post(String id_post){
+        this.id_post = id_post;
+    }
+}
